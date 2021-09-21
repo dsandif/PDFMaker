c38d06688bd43422fce4f39f6106b197102bc88f
Darien <dsis335@icloud.com>
2021-09-21 09:00:01 -0500

    Initial Commit

---
+++ PDFMaker/PDFMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFMakerApp.swift
+//  PDFMaker
+//
+//  Created by Darien Sandifer on 9/21/21.
+//
+
+import SwiftUI
+
+@main
+struct PDFMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
